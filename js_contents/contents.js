9d3b31e8372b60c0626d093ca9e4e03c66a86103
rajsekharsaha <rajs4809342@gmail.com>
2023-09-10 21:09:07 +0530

    ok

---
+++ js_contents/contents.js
@@ -0,0 +1,22 @@
+document.write(`
+
+<!-- contents start -->
+    <div class="content">
+        <ul class="cont-items">
+            <li>
+                <img src="./res/desktop-computer.png" alt="logo" width="50px">
+                <p>My Computer</p>
+            </li>
+            <li>
+                <img src="./res/files-and-folders.png" alt="logo" width="50px">
+                <p>My Files</p>
+            </li>
+            <li>
+                <img src="./res/explorer.png" alt="logo" width="50px">
+                <p>explorer</p>
+            </li>
+        </ul>
+    </div>
+<!-- contents end-->
+
+`)
