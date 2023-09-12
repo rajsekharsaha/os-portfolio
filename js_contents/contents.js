8db8bc31b524bf9928f4da76e1287b10e4b9780d
rajsekharsaha <rajs4809342@gmail.com>
2023-09-12 20:04:18 +0530

    ok

--- js_contents/contents.js
+++ js_contents/contents.js
@@ -4,15 +4,15 @@ document.write(`
     <div class="content">
         <ul class="cont-items">
             <li>
-                <img src="./res/desktop-computer.png" alt="logo" width="50px">
+                <img src="./res/desktop-computer.png" alt="logo" width="50px" class="mycomputer">
                 <p>My Computer</p>
             </li>
             <li>
-                <img src="./res/files-and-folders.png" alt="logo" width="50px">
+                <img src="./res/files-and-folders.png" alt="logo" width="50px" class="file">
                 <p>My Files</p>
             </li>
             <li>
-                <img src="./res/explorer.png" alt="logo" width="50px">
+                <img src="./res/explorer.png" alt="logo" width="50px" class="browser">
                 <p>explorer</p>
             </li>
         </ul>
